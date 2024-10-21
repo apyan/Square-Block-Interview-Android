9f43737982da44b63fc8de3719c928f89af64035
SlyDaemon <apyan7852@gmail.com>
2024-10-21 05:33:55 -0700

    Testing

--- Android/app/src/test/java/com/example/squareinterview/EmployeeListViewModelUnitTest.kt
+++ Android/app/src/test/java/com/example/squareinterview/EmployeeListViewModelUnitTest.kt
@@ -1,19 +1,69 @@
 package com.example.squareinterview
 
+import com.example.squareinterview.model.response.Employee
+import com.example.squareinterview.viewmodel.EmployeeListViewModel
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
 
 class EmployeeListViewModelUnitTest {
 
-//    private lateinit var comicBookViewModel: ComicBookViewModel
-//
-//    @Before
-//    fun setup() {
-//        comicBookViewModel = ComicBookViewModel()
-//    }
-//
-//    @Test
-//    fun check_for_initial_data() {
-//        assertThat(comicBookViewModel.comicBookList.value.isEmpty()).isTrue()
-//    }
+    private lateinit var employeeListViewModel: EmployeeListViewModel
+
+    val employee_0 = Employee(
+        uuid = "",
+        fullName = "Alice",
+        phoneNumber = "",
+        emailAddress = "",
+        biography = "",
+        photoUrlSmall = "",
+        photoUrlLarge = "",
+        team = "C",
+        employeeType = ""
+    )
+
+    val employee_1 = Employee(
+        uuid = "",
+        fullName = "Jake",
+        phoneNumber = "",
+        emailAddress = "",
+        biography = "",
+        photoUrlSmall = "",
+        photoUrlLarge = "",
+        team = "B",
+        employeeType = ""
+    )
+
+    val employee_2 = Employee(
+        uuid = "",
+        fullName = "Bob",
+        phoneNumber = "",
+        emailAddress = "",
+        biography = "",
+        photoUrlSmall = "",
+        photoUrlLarge = "",
+        team = "A",
+        employeeType = ""
+    )
+
+    @Before
+    fun setup() {
+        employeeListViewModel = EmployeeListViewModel()
+    }
+
+    @Test
+    fun check_user_name_sorting() {
+        val secondName = "Bob"
+
+        employeeListViewModel.employeeList.value = listOf(employee_2, employee_1, employee_0)
+        assertThat(employeeListViewModel.sortNameEmployees()[1].fullName == secondName).isTrue()
+    }
+
+    @Test
+    fun check_user_team_sorting() {
+        val secondName = "B"
+
+        employeeListViewModel.employeeList.value = listOf(employee_2, employee_1, employee_0)
+        assertThat(employeeListViewModel.sortTeamEmployees()[1].fullName == secondName).isTrue()
+    }
 }
--- Android/app/src/test/java/com/example/squareinterview/ExampleUnitTest.kt
+++ Android/app/src/test/java/com/example/squareinterview/ExampleUnitTest.kt
@@ -14,18 +14,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-
-//    @Test
-//    fun check_creating_hashcode() {
-//        val createdHash = Constant.createHash()
-//        assertThat(createdHash).isNotNull()
-//    }
-//
-//    @Test
-//    fun check_conversion_hashcode() {
-//        val createdHash = Constant.createHash()
-//        val concatToken = "${Constant.timeStamp}${Constant.API_PRIVATE_KEY}${Constant.API_PUBLIC_KEY}"
-//        assertThat(createdHash != concatToken).isTrue()
-//    }
 }
